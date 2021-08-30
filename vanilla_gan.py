05d165e7b2223a34fa9e2871a8d7db997d9fbdf9
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-08-30 15:52:21 +0900

    error

--- vanilla_gan.py
+++ vanilla_gan.py
@@ -54,3 +54,90 @@ discriminator.summary()
 
 discriminator.compile(loss='binary_crossentropy', optimizer=adam)
 
+# GAN
+discriminator.trainable = False
+gan_input = Input(shape=(NOISE_DIM,))
+x = generator(input=gan_input)
+output = discriminator(x)
+
+gan = Model(gan_input, output)
+gan.summary()
+
+gan.compile(loss='binary_crossentropy', optimizer=adam)
+
+
+
+def get_batches(data, batch_size):
+    batches = []
+    for i in range(int(data.shape[0] // batch_size)):
+        batch = data[i * batch_size: (i + 1) * batch_size]
+        batches.append(batch)
+    return np.asarray(batches)
+
+def visualize_training(epoch, d_losses, g_losses):
+    # 오차에 대한 시각화
+    plt.figure(figsize=(8, 4))
+    plt.plot(d_losses, label='Discriminator Loss')
+    plt.plot(g_losses, label='Generatror Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    print('epoch: {}, Discriminator Loss: {}, Generator Loss: {}'.format(epoch, np.asarray(d_losses).mean(), np.asarray(g_losses).mean()))
+    
+    #샘플 데이터 생성 후 시각화
+    noise = np.random.normal(0, 1, size=(24, NOISE_DIM))
+    generated_images = generator.predict(noise)
+    generated_images = generated_images.reshape(-1, 28, 28)
+    
+    plt.figure(figsize=(8, 4))
+    for i in range(generated_images.shape[0]):
+        plt.subplot(4, 6, i+1)
+        plt.imshow(generated_images[i], interpolation='nearest', cmap='gray')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+BATCH_SIZE = 128
+EPOCHS= 50
+
+
+# discriminator와 gan 모델의 loss 측정을 위한 list 입니다.
+d_losses = []
+g_losses = []
+
+for epoch in range(1, EPOCHS + 1):
+    # 각 배치별 학습
+    for real_images in get_batches(x_train, BATCH_SIZE):
+        # 랜덤 노이즈 생성
+        input_noise = np.random.uniform(-1, 1, size=[BATCH_SIZE, NOISE_DIM])
+        
+        # 가짜 이미지 데이터 생성
+        generated_images = generator.predict(input_noise)
+        
+        # Gan에 학습할 X 데이터 정의
+        x_dis = np.concatenate([real_images, generated_images])
+        
+        # Gan에 학습할 Y 데이터 정의
+        y_dis = np.zeros(2 * BATCH_SIZE)
+        y_dis[:BATCH_SIZE] = 0.9
+        
+        # Discriminator 훈련
+        discriminator.trainable = True
+        d_loss = discriminator.train_on_batch(x_dis, y_dis)
+        
+        # Gan 훈련
+        noise = np.random.uniform(-1, 1, size=[BATCH_SIZE, NOISE_DIM])
+        y_gan = np.ones(BATCH_SIZE)
+        
+        # Discriminator의 판별 학습을 방지합니다
+        discriminator.trainable = False
+        g_loss = gan.train_on_batch(noise, y_gan)
+        
+    d_losses.append(d_loss)
+    g_losses.append(g_loss)
+    
+    if epoch == 1 or epoch % 5 == 0:
+        visualize_training(epoch, d_losses, g_losses)
